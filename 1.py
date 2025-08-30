256fdfbd8dcb50cc97fdd467b336e82aefb8e24a
patissierMongs <patissierMongs@gmail.com>
2025-08-30 23:47:09 +0900

    python

---
+++ 1.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+arr=np.random.rand(30)
+
+print(arr)
+print ("hello")
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return arr
+
+
+print(bubble_sort(arr))
